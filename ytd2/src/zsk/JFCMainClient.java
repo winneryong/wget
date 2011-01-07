1b2d9a2e1ef91e3fd78835c44634cb7c11a6c6a4
stefankaerst <stefankaerst@d2ee303e-525f-44b2-a054-c45e44779f53>
2011-01-07 07:48:43 +0000

    better removing of html tags from dropped in URL
    git-svn-id: https://ytd2.svn.sourceforge.net/svnroot/ytd2@32 d2ee303e-525f-44b2-a054-c45e44779f53

--- ytd2/src/zsk/JFCMainClient.java
+++ ytd2/src/zsk/JFCMainClient.java
@@ -77,7 +77,7 @@ import javax.swing.event.DocumentListener;
  * java code could be easily converted to Java 1.4.2
  */
 public class JFCMainClient extends JFrame implements ActionListener, WindowListener, DocumentListener, ChangeListener, DropTargetListener {
-	public static final String szVersion = "V20110103_1800 by MrKnödelmann";
+	public static final String szVersion = "V20110107_0847 by MrKnödelmann";
 	
 	private static final long serialVersionUID = 6791957129816930254L;
 
