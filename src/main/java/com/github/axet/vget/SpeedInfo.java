f12c79dfcecdf9e8866a890f6f8a59c26b73818c
Alexey Kuznetsov <ak@axet.ru>
2012-04-20 12:33:10 +0200

    fix speed info

--- src/main/java/com/github/axet/vget/SpeedInfo.java
+++ src/main/java/com/github/axet/vget/SpeedInfo.java
@@ -9,7 +9,8 @@ public class SpeedInfo {
         public long current;
         // current time
         public long now;
-        // start block? used to mark block after download has been altered / restarted
+        // start block? used to mark block after download has been altered /
+        // restarted
         public boolean start;
 
         public Sample() {
@@ -36,8 +37,85 @@ public class SpeedInfo {
     public static final int SAMPLE_LENGTH = 2000;
     public static final int SAMPLE_MAX = 20;
 
+    // start sample use to calculate average speed
+    Sample start = null;
+
     public SpeedInfo() {
     }
+
+    /**
+     * Start calculate speed from 'current' bytes downloaded
+     * 
+     * @param current
+     */
+    public void start(long current) {
+        Sample s = new Sample(current);
+        s.start = true;
+        start = s;
+        add(s);
+    }
+
+    /**
+     * step download process with 'current' bytes downloaded
+     * 
+     * @param current
+     */
+    public void step(long current) {
+        long now = System.currentTimeMillis();
+
+        long lastUpdate = getLastUpdate();
+        if (lastUpdate + SAMPLE_LENGTH < now) {
+            add(new Sample(current, now));
+        }
+    }
+
+    /**
+     * Current download speed
+     * 
+     * @return bytes per second
+     */
+    public int getCurrentSpeed() {
+        if (samples.size() < 2)
+            return 0;
+
+        // [s1] [s2] [EOF]
+        Sample s1 = samples.get(samples.size() - 2);
+        Sample s2 = samples.get(samples.size() - 1);
+
+        long current = s2.current - s1.current - start.current;
+        long time = s2.now - s1.now;
+
+        if (time == 0)
+            return 0;
+
+        return (int) (current * 1000 / time);
+    }
+
+    /**
+     * Average speed from start download
+     * 
+     * @return bytes per second
+     */
+    public int getAverageSpeed() {
+        Sample s2 = samples.get(samples.size() - 1);
+
+        long current = s2.current - start.current;
+        long time = s2.now - start.now;
+
+        return (int) (current * 1000 / time);
+    }
+
+    public int getSamples() {
+        return samples.size();
+    }
+
+    public Sample getSample(int index) {
+        return samples.get(index);
+    }
+
+    //
+    // protected
+    //
 
     Sample getStart() {
         for (int i = samples.size() - 1; i >= 0; i--) {
@@ -46,13 +124,20 @@ public class SpeedInfo {
                 return s;
         }
 
-        throw new RuntimeException("start sample not found");
+        return null;
     }
 
     void add(Sample s) {
-        Sample start = getStart();
-        if (start.current > s.current) {
-            s.start = true;
+        // check if we have broken / restarted download. check if here some
+        // samples
+        if (samples.size() > 1) {
+            Sample s1 = samples.get(samples.size() - 1);
+            // check if last download 'current' stands before current 'current'
+            // download
+            if (s1.current > s.current) {
+                s.start = true;
+                start = s;
+            }
         }
 
         samples.add(s);
@@ -67,67 +152,5 @@ public class SpeedInfo {
 
         Sample s = samples.get(samples.size() - 1);
         return s.now;
-    }
-
-    public void start(long current) {
-        Sample s = new Sample(current);
-        s.start = true;
-        add(s);
-    }
-
-    public void step(long current) {
-        long now = System.currentTimeMillis();
-
-        long lastUpdate = getLastUpdate();
-        if (lastUpdate + SAMPLE_LENGTH < now) {
-            add(new Sample(current, now));
-        }
-    }
-
-    /**
-     * bytes per second
-     * 
-     * @return
-     */
-    public int getCurrentSpeed() {
-        if (samples.size() < 2)
-            return 0;
-
-        Sample start = getStart();
-
-        // start block
-        Sample s = start;
-
-        // [s1] [s2] [EOF]
-        Sample s1 = samples.get(samples.size() - 2);
-        Sample s2 = samples.get(samples.size() - 1);
-
-        long current = s2.current - s1.current - s.current;
-        long time = s2.now - s1.now;
-
-        if (time == 0)
-            return 0;
-
-        return (int) (current * 1000 / time);
-    }
-
-    public int getAverageSpeed() {
-        Sample start = getStart();
-
-        Sample s1 = start;
-        Sample s2 = samples.get(samples.size() - 1);
-
-        long current = s2.current - s1.current;
-        long time = s2.now - s1.now;
-
-        return (int) (current * 1000 / time);
-    }
-
-    public int getSamples() {
-        return samples.size();
-    }
-
-    public Sample getSample(int index) {
-        return samples.get(index);
     }
 }
