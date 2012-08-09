45e665df9bd9b719b406cf64121695a1944df25f
Alexey Kuznetsov <ak@axet.ru>
2012-08-09 16:42:04 +0400

    add controlled download demo

--- src/main/java/com/github/axet/wget/WGet.java
+++ src/main/java/com/github/axet/wget/WGet.java
@@ -224,9 +224,35 @@ public class WGet {
     }
 
     public static void main(String[] args) {
+        // simple example. direct one call download
+
         try {
             WGet w = new WGet(new URL("http://www.dd-wrt.com/routerdb/de/download/D-Link/DIR-300/A1/ap61.ram/2049"),
                     new File("/Users/axet/Downloads/"));
+            w.download();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        // application controlled download with start / resume functionality
+
+        try {
+            final DownloadInfo info = new DownloadInfo(new URL(
+                    "http://www.dd-wrt.com/routerdb/de/download/D-Link/DIR-300/A1/ap61.ram/2049"));
+            info.extract();
+
+            Runnable notify = new Runnable() {
+
+                @Override
+                public void run() {
+                    float progress = info.getCount() / info.getLength();
+                    System.out.println("progress " + progress);
+                }
+            };
+
+            AtomicBoolean stop = new AtomicBoolean(false);
+
+            WGet w = new WGet(info, new File("/Users/axet/Downloads/"), stop, notify);
             w.download();
         } catch (MalformedURLException e) {
             e.printStackTrace();
